b968f3796ee720a9b73e200ecf20a4b6beff483c
Eder Henrique <ederhscc@gmail.com>
2023-11-23 09:43:11 -0300

    updated TaskList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./App.module.css";
 
 // components
@@ -7,18 +7,27 @@ import Footer from "./components/Footer";
 import TaskForm from "./components/TaskForm";
 import TaskList from "./components/TaskList";
 
+// Interfaces
+import { ITask } from "./interfaces/Task";
+
 function App() {
+  const [taskList, setTaskList] = useState<ITask[]>([]);
+
   return (
     <div>
       <Header />
       <main className={styles.main}>
         <div>
           <h2>O que você vai fazer?</h2>
-          <TaskForm btnText="Criar Tarefa"/>
+          <TaskForm
+            btnText="Criar Tarefa"
+            taskList={taskList}
+            setTaskList={setTaskList}
+          />
         </div>
         <div>
           <h2>Suas tarefas</h2>
-          <TaskList />
+          <TaskList taskList={taskList}/>
         </div>
       </main>
       <Footer />
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,12 +1,31 @@
 import React from "react";
+import { ITask } from "../interfaces/Task";
+import styles from "./TaskList.module.css";
 
-interface Props {}
+interface Props {
+  taskList: ITask[];
+}
 
-const TaskList = (props: Props) => {
+const TaskList = ({ taskList }: Props) => {
   return (
-    <div>
-      <p>lista de tarefa</p>
-    </div>
+    <>
+      {taskList.length > 0 ? (
+        taskList.map((task) => (
+          <div key={task.id}>
+              <div>
+                <h4>{task.title}</h4>
+                <p>Dificuldade: {task.difficulty}</p>
+              </div>
+              <div>
+                <i className="bi bi-pencil"></i>
+                <i className="bi bi-trash"></i>
+              </div>
+          </div>
+        ))
+      ) : (
+        <p>Não há tarefas cadastradas</p>
+      )}
+    </>
   );
 };
 
